d658d463ad47dfddc3eeab38f0d59a094fe27738
Björn Scheppler <scep@zhaw.ch>
2020-11-16 15:51:35 +0100

    Musterlösung übernommen Aufgabe 8

---
+++ src/main/java/ch/zhaw/gpi/creditorprocess/GetOrderAndCreditorDataDelegate.java
@@ -0,0 +1,41 @@
+package ch.zhaw.gpi.creditorprocess;
+
+import javax.inject.Named;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.json.JSONObject;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Named("getOrderAndCreditorDataAdapter")
+public class GetOrderAndCreditorDataDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        Long referenceNr = (Long) execution.getVariable("referenceNr");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8070/api/orders/search/findByReferenceNumber?referenceNumber={referenceNr}", HttpMethod.GET, null, String.class, referenceNr);
+
+        Boolean orderFound = response.getStatusCode().equals(HttpStatus.OK);
+
+        execution.setVariable("orderFound", orderFound);
+
+        if(orderFound){
+            JSONObject orderAsJsonObject = new JSONObject(response.getBody());
+            execution.setVariable("orderAmount", orderAsJsonObject.getLong("amount"));
+            execution.setVariable("costCenterMgr", orderAsJsonObject.getString("cstCtMgr"));
+            String creditorUrl = orderAsJsonObject.getJSONObject("_links").getJSONObject("creditor").getString("href");
+            response = restTemplate.exchange(creditorUrl, HttpMethod.GET, null, String.class);
+
+            JSONObject creditorAsJsonObject = new JSONObject(response.getBody());
+            execution.setVariable("creditorOrderCount", creditorAsJsonObject.getInt("ordersCnt"));
+            execution.setVariable("creditorInvoiceReclamationCount", creditorAsJsonObject.getInt("invoicingReclamationCnt"));
+        }
+    }
+    
+}
